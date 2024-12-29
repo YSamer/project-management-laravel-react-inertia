c7a87005cefc3ca480ab065ab148ebcee5f82312
YSamer <ysamer2525@gmail.com>
2024-12-29 13:57:39 +0200

    Edit Routes Versions

---
+++ routes/v3/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\v2\DashboardController;
+use App\Http\Controllers\v2\ProfileController;
+use App\Http\Controllers\v2\ProjectController;
+use App\Http\Controllers\v2\TaskController;
+use App\Http\Controllers\v2\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v3')->group(function () {
+    Route::redirect('/', '/v3/dashboard');
+
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('projects', ProjectController::class);
+        Route::resource('tasks', TaskController::class);
+        Route::get('my-tasks', [TaskController::class, 'myTasks'])
+            ->name('tasks.myTasks');
+        Route::resource('users', UserController::class);
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+});
+
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,9 @@
 <?php
 
-use App\Http\Controllers\v2\DashboardController;
-use App\Http\Controllers\v2\ProfileController;
-use App\Http\Controllers\v2\ProjectController;
-use App\Http\Controllers\v2\TaskController;
-use App\Http\Controllers\v2\UserController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+
+
+require __DIR__ . '/v2/auth.php';
+require __DIR__ . '/v2/web.php';
 
 Route::redirect('/', '/v2/dashboard');
-Route::prefix('v2')->group(function () {
-    Route::redirect('/', '/v2/dashboard');
-
-    Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-        Route::resource('projects', ProjectController::class);
-        Route::resource('tasks', TaskController::class);
-        Route::get('my-tasks', [TaskController::class, 'myTasks'])
-            ->name('tasks.myTasks');
-        Route::resource('users', UserController::class);
-    });
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-    require __DIR__ . '/auth.php';
-});
-
